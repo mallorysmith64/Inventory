31d1b509ebcd689592fd53ab1a0e31067dffa7da
Mars <mallorysmith6464@gmail.com>
2019-09-18 15:43:28 -0400

    added migration for updates to list table

---
+++ Migrations/20190918194239_UpdatedListTable.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace inventory.Migrations
+{
+    public partial class UpdatedListTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "SKU",
+                table: "Items",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Price",
+                table: "Items",
+                nullable: false,
+                oldClrType: typeof(int));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "SKU",
+                table: "Items",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Price",
+                table: "Items",
+                nullable: false,
+                oldClrType: typeof(double));
+        }
+    }
+}
